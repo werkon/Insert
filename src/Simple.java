07f5783d24d7f3ba45e46e552fa3759321cf52c1
Werner König <w.koenig@funkedigital.de>
2023-05-23 17:20:46 +0200

    simple

--- src/Simple.java
+++ src/Simple.java
@@ -2,15 +2,31 @@
 import java.util.Scanner;
 
 import de.esnecca.multi.History;
+import de.esnecca.multi.SimpleThink;
 
 public class Simple {
     public static void main(String[] args) throws Exception {
 
-        History history = new History(7, 6, 2, 4);
+        History history = new History(4, 4, 2, 3);
         Scanner keyboard = new Scanner(System.in);
 
         while (true) {
             history.print();
+
+            SimpleThink simpleThink = new SimpleThink(history);
+
+            if( !history.isFull(0)){
+                System.out.println( "1:" + simpleThink.think(0) );
+            }
+            if( !history.isFull(1)){
+                System.out.println( "2:" + simpleThink.think(1) );
+            }
+            if( !history.isFull(2)){
+                System.out.println( "3:" + simpleThink.think(2) );
+            }
+            if( !history.isFull(3)){
+                System.out.println( "4:" + simpleThink.think(3) );
+            }
 
             String input = keyboard.nextLine();
 
