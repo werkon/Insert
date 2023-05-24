17e78cb40047ab78e2e4a2cc6e1a6157cceecacf
Werner König <werner.koenig@funkemedien.de>
2023-05-24 21:08:22 +0200

    bi

--- src/Simple.java
+++ src/Simple.java
@@ -9,7 +9,7 @@ import de.esnecca.multi.SimpleThink;
 public class Simple {
     public static void main(String[] args) throws Exception {
 
-        History history = new History(4, 4, 2, 4);
+        History history = new History(7, 6, 2, 4);
 
 
 
@@ -25,19 +25,19 @@ public class Simple {
 
             SimpleThink simpleThink = new SimpleThink(history);
 
-            if( !history.isFull(0)){
-                System.out.println( "1:" + simpleThink.think(0) );
-            }
-            if( !history.isFull(1)){
-                System.out.println( "2:" + simpleThink.think(1) );
-            }
-            if( !history.isFull(2)){
-                System.out.println( "3:" + simpleThink.think(2) );
-            }
-            if( !history.isFull(3)){
-                System.out.println( "4:" + simpleThink.think(3) );
-            }
-            // if( !history.isFull(4)){
+            // if( !history.isFull(0)){
+            //     System.out.println( "1:" + simpleThink.think(0) );
+            // }
+            // if( !history.isFull(1)){
+            //     System.out.println( "2:" + simpleThink.think(1) );
+            // }
+            // if( !history.isFull(2)){
+            //     System.out.println( "3:" + simpleThink.think(2) );
+            // }
+            // if( !history.isFull(3)){
+            //     System.out.println( "4:" + simpleThink.think(3) );
+            // }
+            // // if( !history.isFull(4)){
             //     //System.out.println( "5:" + simpleThink.think(4) );
             // }
 
