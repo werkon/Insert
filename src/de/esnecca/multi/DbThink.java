31972978d9adcc537353be1bbef20f2c1eb3eae9
Werner König <w.koenig@funkedigital.de>
2023-06-14 12:52:53 +0200

    initial think

--- src/de/esnecca/multi/DbThink.java
+++ src/de/esnecca/multi/DbThink.java
@@ -64,14 +64,38 @@ public class DbThink extends Thread {
     }
 
     public int think(int x) throws SQLException {
-        // if (game.test(x)) {
-        // return 1;
-        // }
+
+        if (history.test(x)) {
+            return 1;
+        }
+
+        history.insert(x);
+
+        for (int i = 0; i < history.getWidth(); ++i) {
+            if (!history.isFull(i)) {
+                if (history.test(i)) {
+                    history.remove(x);
+                    return -2;
+                }
+            }
+        }
+
+        history.remove(x);
+
+        return _think(x);
+    }
+
+    private int _think(int x) throws SQLException {
+
         if (history.getInserted() >= history.getSize() - 1) {
             return 0;
         }
 
+        // max 40
+
         history.insert(x);
+
+        // max 41
 
         for (int i = 0; i < history.getWidth(); ++i) {
             if (!history.isFull(i)) {
@@ -126,7 +150,7 @@ public class DbThink extends Thread {
         boolean unentschieden = false;
         for (int i = 0; i < history.getWidth(); ++i) {
             if (!history.isFull(i)) {
-                int r = think(i);
+                int r = _think(i);
                 if (r > 0) {
                     if (kleinstesPositivesR == 0 || r < kleinstesPositivesR) {
                         kleinstesPositivesR = r;
